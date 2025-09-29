bb87b3f3f743aaf2fb64b772f26e721caa4e771d
Jannis Domann <public@domann.cc>
2025-09-29 22:20:24 +0200

    add base functionality to ckernel

--- ckernel.c
+++ ckernel.c
@@ -1,3 +1,53 @@
+void kClearScreen() {
+	char* videoBufferStart = (char*)0xb8000;
+	char* videoBuffer = videoBufferStart;
+	char cols = 0x50;	// 80 cols
+	char rows = 0x19;	// 25 rows
+	char bg = 0x00;
+	char fg = 0x07;
+
+	while (videoBuffer < (char*)videoBufferStart+cols*rows*0x02) {
+		*videoBuffer++ = 0x20; // space
+		*videoBuffer++ = (bg << 0x04) & 0xf0 | fg & 0x0f;
+	}
+}
+
+void kPrintf(char* txt, char row, char attrib) {
+	char* videoBufferStart = (char*)0xb8000;
+	char cols = 0x50;	// 80 cols
+	char* videoBuffer = (char*)videoBufferStart+row*cols*0x02;
+
+	while (*txt != 0) {
+		if (*txt == '\n') {
+			++row;
+			++txt;
+			videoBuffer = (char*)videoBufferStart+row*cols*0x02;
+			continue;
+		}
+		*videoBuffer++ = (char)*txt;
+		*videoBuffer++ = attrib;
+		++txt;
+	}
+}
+
+void outb(unsigned int port, char value) {
+	__asm__ volatile ("outb %%al,%%dx" :: "d" (port), "a" (value));
+}
+
+void kUpdateCursor(char row, char col) {
+	unsigned short pos = row*0x50+col;
+
+	outb(0x3d4, 0x0e);
+	outb(0x3d5, (char)((pos>>0x08)&0xff));
+	outb(0x3d4, 0x0f);
+	outb(0x3d5, (char)(pos&0xff));
+}
+
 int main() {
+	kClearScreen();
+	kPrintf("Hello World!\nHow are you?", 0x00, 0x02);
+	kUpdateCursor(0x02, 0x00);
+
     return 0;
 }
+
