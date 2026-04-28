3199a73ed291e7eca9b2c6710f57ebd2fd57994e
Jannis Domann <public@domann.cc>
2026-04-28 21:34:48 +0200

    implement compare function k_memcmp()

--- src/kernel/include/mem.h
+++ src/kernel/include/mem.h
@@ -12,6 +12,7 @@ void k_memcpy_u16(const void* dest, const void* src, uint64_t count);
 void k_memset_u16(const void* dest, uint16_t value, uint64_t count);
 void k_memcpy_u64(const void* dest, const void* src, uint64_t count);
 void k_memset_u64(const void* dest, uint64_t value, uint64_t count);
+uint32_t k_memcmp(const void* s1, const void* s2, uint32_t n);
 
 #ifdef __cplusplus
 }
--- src/kernel/mem/mem.c
+++ src/kernel/mem/mem.c
@@ -45,3 +45,11 @@ void k_memset_u64(const void* dest, uint64_t value, uint64_t count) {
 	}
 }
 
+uint32_t k_memcmp(const void* s1, const void* s2, uint32_t n) {
+    const uint8_t* p1 = (uint8_t*)s1;
+    const uint8_t* p2 = (uint8_t*)s2;
+    for (uint32_t i = 0; i < n; i++) {
+        if (p1[i] != p2[i]) return p1[i] - p2[i];
+    }
+    return 0;
+}
